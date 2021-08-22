e917e54b95a1c610e8d101b54caa6fb437f9b3d0
Vyacheslav <ggg9gg6@yandex.ru>
2021-08-22 22:25:46 +0300

    HTML SCSS CSS JS return to start icon

--- src/js/script.js
+++ src/js/script.js
@@ -107,4 +107,19 @@ $(document).ready(function(){
         });
         return false;
     });
+
+    //Scroll
+    $(window).scroll(function(){
+        if($(this).scrollTop()>1150){
+            $('.pageup').fadeIn();
+        }
+        else{
+            $('.pageup').fadeOut();
+        }
+    });
+    $("a[href^='#']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
 });
